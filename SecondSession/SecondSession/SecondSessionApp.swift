73b2dd001a466121a96b29087fbeec660632446d
MinyoungKIM <103025017+pig1606@users.noreply.github.com>
2022-11-24 04:34:36 +0900

    [ADD] New Xcode File

---
+++ SecondSession/SecondSession/SecondSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondSessionApp.swift
+//  SecondSession
+//
+//  Created by Minyoung Kim on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SecondSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
