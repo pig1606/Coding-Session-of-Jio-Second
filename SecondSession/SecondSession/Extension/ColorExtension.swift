606fb50d7301ec4442a1c3298b19eab9470c8a92
pig1606 <pig1606@knu.ac.kr>
2022-11-24 04:56:50 +0900

    [ADD] Color Assets and Color Extension

---
+++ SecondSession/SecondSession/Extension/ColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  ColorExtension.swift
+//  SecondSession
+//
+//  Created by Minyoung Kim on 2022/11/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let primaryPurple = Color("primaryPurple")
+    static let secondaryPurple = Color("secondaryPurple")
+}
